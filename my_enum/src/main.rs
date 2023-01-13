b1f7bde0fa799588fe89d956b04f862711f9f95a
elzup <guild0105@gmail.com>
2023-01-13 09:33:27 +0900

    add my_enum

---
+++ my_enum/src/main.rs
@@ -0,0 +1,18 @@
+
+enum Heating {Weak, Strong }
+enum Running { Cooling, Heating(Heating), Dry }
+enum State { Off, Running(Running) }
+
+fn main() {
+    // dbg!("{:?}", Disjoint::Alpha(Alpha::A));
+    let state = State::Running(Running::Heating(Heating::Strong));
+
+    match state {
+        State::Off => println!("a"),
+        State::Running(Running::Cooling) => println!("a"),
+        State::Running(Running::Heating(Heating::Strong)) => println!("a"),
+        State::Running(Running::Heating(_)) => println!("a"),
+        State::Running(_) => println!("a"),
+        _ => {}
+    };
+}
